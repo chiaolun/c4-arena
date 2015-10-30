6727a5ba7eeefc303e7f2c7983e16d24d5b2974f
Chiao Cheng <chiaolun@gmail.com>
2015-10-29 23:25:48 -0400

    wip

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.taoensso/timbre "4.1.4"]
+                 [compojure "1.4.0"]
                  [aleph "0.4.0"]
-                 [com.taoensso/timbre "4.1.4"]]
+                 [cheshire "5.5.0"]]
   :plugins [[cider/cider-nrepl "0.9.1"]]
   :main ^:skip-aot c4-arena.core
   :target-path "target/%s"
--- src/c4_arena/core.clj
+++ src/c4_arena/core.clj
@@ -1,25 +1,74 @@
 (ns c4-arena.core
   (:gen-class)
   (:require
+   [clojure.core
+    [async :refer [go go-loop <! >! chan]]]
+   [taoensso.timbre :as tb]
    [manifold
-    [stream :as s]
+    [stream :as st]
     [deferred :as d]]
    [aleph
-    [http :as http]
-    [tcp :as tcp]]
-   [taoensso.timbre :as tb]))
+    [http :as http]]
+   [compojure.core
+    :refer [defroutes GET]]
+   [cheshire.core :refer [parse-string generate-string]]))
 
-;;; TCP connection for telnet interface
-(defn text-handler [s info]
-  (let [state (atom nil)]
-    (tb/info info)
-    (s/connect s s)))
+(defonce matcher (atom nil))
+(defonce awaiting-match (atom nil))
+
+(defn matcher-init []
+  (let [ch (chan)]
+    (go-loop []
+      (when-let [{:keys [id chs latch]} (<! ch)]
+
+        (recur)))
+    (reset! matcher ch)))
+
+;;; Game handler
+(defn game-handler [[ch-in ch-out :as chs]]
+  (go-loop []
+    (when-let [msg (<! ch-in)]
+      (let [{:keys [type id]} msg]
+        (cond
+          (and (= type "start") id)
+          (let [latch (chan)]
+            (>! @matcher {:id id :chs chs :latch latch})
+            ;; Waits here until game ends
+            (<! latch))
+          (do (>! ch-out {:type "ignored" :msg msg})
+              (recur)))))))
+
+;;; Game loop
+(defn game-init [s]
+  (let [ch-in (chan)
+        ch-out (chan)
+        chs [ch-in ch-out]]
+    ;; Incoming messages
+    (st/connect (st/map #(parse-string % true) s) ch-in)
+    ;; Outgoing messages
+    (st/connect (st/map generate-string ch-out) s)
+    ;; Event loop for the connection
+    (go
+      ;; Keep waiting to start new games if channels are still alive
+      (<! (game-handler chs))
+      (st/close! s))))
 
 ;;; Websocket connection for the game protocol
 (defn game-handler [req]
   (let [s @(http/websocket-connection req)]
-    (s/connect s s)))
+    (game-init s)))
+
+;;; Websocket connection for the firehose, carrying all state updates
+;;; for the whole server
+(defn firehose-handler [req]
+  (let [s @(http/websocket-connection req)]
+    ;; TODO
+    ))
+
+(defroutes app
+  (GET "/firehose" [] #'firehose-handler)
+  (GET "/" [] #'game-handler))
 
 (defn -main [& args]
-  (http/start-server handler {:port 8001})
-  (tcp/start-server #'text-handler {:port 8002}))
+  (matcher-init)
+  (http/start-server #'game-handler {:port 8001}))
