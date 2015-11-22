cb404ae8f99581ab2781cda1cf10a626c8729ceb
Chiao Cheng <chiaolun@gmail.com>
2015-11-22 10:11:15 -0500

    NN representation to one-hot

--- clients/neuralq.py
+++ clients/neuralq.py
@@ -13,20 +13,20 @@ state_dim = ncols * nrows
 # http://outlace.com/Reinforcement-Learning-Part-3/
 
 def standardize(state, side):
-    standard_state = []
-    for x in state:
+    N = len(state)
+    standard_state = np.zeros((N,3))
+    for i, x in enumerate(state):
         if x == 0:
-            val = 0
+            standard_state[i,0] = 1.
         elif x == side:
-            val = 1
+            standard_state[i,1] = 1.
         else:
-            val = 2
-        standard_state.append(val)
-    return standard_state
+            standard_state[i,2] = 1.
+    return standard_state.reshape(1,state_dim*3)
 
 def valid_columns(state):
     return [j for j in range(ncols)
-            if any(state[i] == 0 for i in range(j * nrows, (j + 1) * nrows))]
+            if any(state[0, 3 * i] == 1 for i in range(j * nrows, (j + 1) * nrows))]
 
 class NeuralQ():
     def __init__(self, epsilon = 0.01, gamma = 1.):
@@ -34,16 +34,16 @@ class NeuralQ():
         self.gamma = gamma
 
         model = Sequential()
-        model.add(Dense(30, init='lecun_uniform', input_shape=(state_dim,)))
-        model.add(Activation('relu'))
-        #model.add(Dropout(0.2)) I'm not using dropout, but maybe you wanna give it a try?
+        model.add(Dense(30, init='lecun_uniform', input_shape=(state_dim*3,)))
+        model.add(Activation('tanh'))
+        #model.add(Dropout(0.2))
 
         model.add(Dense(15, init='lecun_uniform', input_shape=(state_dim,)))
-        model.add(Activation('relu'))
-        #model.add(Dropout(0.2)) I'm not using dropout, but maybe you wanna give it a try?
+        model.add(Activation('tanh'))
+        #model.add(Dropout(0.2))
 
         model.add(Dense(ncols, init='lecun_uniform'))
-        model.add(Activation('linear')) #linear output so we can have range of real-valued outputs
+        model.add(Activation('tanh')) #linear output so we can have range of real-valued outputs
 
         rms = RMSprop()
         model.compile(loss='mse', optimizer=rms)
@@ -59,7 +59,7 @@ class NeuralQ():
         state = np.array(state)
         #We are in state S
         #Let's run our Q function on S to get Q values for all possible actions
-        qval = model.predict(state.reshape(1,state_dim), batch_size=1)
+        qval = model.predict(state, batch_size=1)
         qval_allowed = np.empty(qval.shape)
         qval_allowed[:] = np.NAN
         valids = valid_columns(state)
@@ -85,13 +85,13 @@ class NeuralQ():
                 new_state = np.array(new_state)
 
                 #Get max_Q(S',a)
-                newQ = model.predict(new_state.reshape(1,state_dim), batch_size=1)
+                newQ = model.predict(new_state, batch_size=1)
                 maxQ = np.max(newQ)
 
                 update = (reward + (gamma * maxQ))
 
             y[0][action] = update #target output
 
-            model.fit(state.reshape(1,state_dim), y, batch_size=1, nb_epoch=1, verbose=1)
+            model.fit(state, y, batch_size=1, nb_epoch=1, verbose=1)
 
         return action, observe_reward
