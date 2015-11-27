b8a8326a84e5f1765e173f24c5c6d600adcc50a6
Chiao Cheng <chiaolun@gmail.com>
2015-11-27 03:52:49 -0500

    architecture tweaking

--- clients/neuralq.py
+++ clients/neuralq.py
@@ -40,13 +40,13 @@ class NeuralQ():
         self.batch_size = 50
 
         model = Sequential()
-        model.add(Dense(30, init='lecun_uniform', input_shape=(state_dim*3,)))
+        model.add(Dense(80, init='lecun_uniform', input_shape=(state_dim*3,)))
         model.add(Activation('relu'))
         # model.add(Dropout(0.5))
 
-        # model.add(Dense(15, init='lecun_uniform', input_shape=(state_dim,)))
-        # model.add(Activation('relu'))
-        # # model.add(Dropout(0.5))
+        model.add(Dense(20, init='lecun_uniform'))
+        model.add(Activation('relu'))
+        # model.add(Dropout(0.5))
 
         model.add(Dense(ncols, init='lecun_uniform'))
         model.add(Activation('linear')) #linear output so we can have range of real-valued outputs
