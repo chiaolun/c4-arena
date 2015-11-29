111a2f104e249d9463c385a1c720970a2dbcca95
Chiao Cheng <chiaolun@gmail.com>
2015-11-28 23:03:47 -0500

    -

--- clients/neuralq.py
+++ clients/neuralq.py
@@ -2,7 +2,7 @@
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import RMSprops, SGD
+from keras.optimizers import RMSprop, SGD
 import numpy as np
 import random
 import os.path
