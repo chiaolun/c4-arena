bdf57d228a380a4b466aaa7147eea7d888e48cfa
Chiao Cheng <chiaolun@gmail.com>
2015-12-02 08:17:45 -0500

    -

--- src/c4_arena/players/C4TrompGame.clj
+++
@@ -1,5 +0,0 @@
-(ns c4-arena.players.C4TrompGame
-  (:gen-class))
-
-(defn toString
-  [this] "Hello World!")
---
+++ src/c4_arena/players/C4TrompState.clj
@@ -0,0 +1,5 @@
+(ns c4-arena.players.C4TrompState
+  (:gen-class
+   :main False
+   :extends aima.core.environment.connectfour.ConnectFourState
+   :exposes-methods {dropDisk superDropDisk}))
